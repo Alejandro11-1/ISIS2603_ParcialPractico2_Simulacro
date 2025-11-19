26aaa0b9c48c9b22b88006e32cd1a774d3e63454
Alejandro Gordillo <a.gordillor@uniandes.edu.co>
2025-11-18 21:46:37 -0500

    punto 4

--- src/app/recipe/recipe-detail/recipe-detail.component.ts
+++ src/app/recipe/recipe-detail/recipe-detail.component.ts
@@ -2,6 +2,8 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Recipe } from '../Recipe';
 import { ActivatedRoute } from '@angular/router';
 import { RecipeService } from '../recipe.service';
+import { Ingredient } from '../../ingredient/Ingredient';
+
 
 @Component({
   selector: 'app-recipe-detail',
@@ -10,15 +12,30 @@ import { RecipeService } from '../recipe.service';
   styleUrl: './recipe-detail.component.css',
 })
 export class RecipeDetailComponent implements OnInit {
-  @Input() recipe: any;
+  @Input() recipe: Recipe | any;
+  maxIngredient?: Ingredient;
 
   constructor(private route: ActivatedRoute, private recipeService: RecipeService) {}
   ngOnInit(): void {
-    const id = this.route.snapshot.paramMap.get('id');
-    if (id) {
-      this.recipeService.getRecipe(Number(id)).subscribe((recipe) => {
-        this.recipe = recipe;
-      });
-    }
+  const id = this.route.snapshot.paramMap.get('id');
+
+  if (id) {
+    this.recipeService.getRecipe(Number(id)).subscribe((recipe) => {
+      this.recipe = recipe;
+      this.maxIngredient = this.findMaxIngredient(recipe.ingredientes);
+    });
   }
+}
+
+//función auxiliar
+private findMaxIngredient(ingredients: Ingredient[]): Ingredient | undefined {
+  if (!ingredients || ingredients.length === 0) {
+    return undefined;
+  }
+
+  // Ignoramos unidades: solo comparamos la cantidad numérica
+  return ingredients.reduce((max, current) =>
+    current.cantidad > max.cantidad? current : max
+  );
+}
 }
